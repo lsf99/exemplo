6cae3e3975e65c9e9867dea7e2288f29d92a2691
lsf99 <80858383+lsf99@users.noreply.github.com>
2021-04-17 19:06:11 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    float n1,n2,m;
+    printf("Digite os valores: ");
+    scanf("%f", &n1);
+    scanf("%f", &n2);
+    m = (n1+n2)/2;
+    printf("%f", m);
+
+    return 0;
+}
